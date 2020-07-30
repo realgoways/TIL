089c1208ae39f07f4ec441a16d5f5c3e0e37ff88
w.w <w.w@navercorp.com>
2020-07-30 18:20:50 +0900

    deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'realgoways-TIL',
+  description: 'Today I Learned',
+  base: "/TIL/",
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' }
+    ]
+  }
+}
